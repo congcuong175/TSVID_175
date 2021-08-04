7a1bef34e9f684b6e694784bf23dcda32d7cdd24
Đỗ Tôn <67794492+thanhtonvk@users.noreply.github.com>
2021-08-04 15:20:57 +0700

    .

---
+++ app/src/main/java/com/example/tsvid_175/Activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.tsvid_175.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tsvid_175.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
